7ddc6961ea0f927d80a6b3a3141ba5b80acae6d8
George <veljanovskigjore@gmail.com>
2021-09-13 19:50:23 +0200

    Automation day 2 finished

--- src/main/java/com/company/PeopleApiClient.java
+++ src/main/java/com/company/PeopleApiClient.java
@@ -1,11 +1,13 @@
 package com.company;
 
+import netscape.javascript.JSObject;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
@@ -13,6 +15,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 
 import javax.net.ssl.SSLContext;
 
@@ -92,6 +95,42 @@ public class PeopleApiClient {
         response.setEntity(newEntity);
 
         return response;
+
+    }
+    public HttpResponse createPerson() throws Exception{
+
+        Header contentType = new BasicHeader(HttpHeaders.CONTENT_TYPE,"application/json");
+
+        SSLContext sslContext = SSLContextBuilder
+                .create()
+                .loadTrustMaterial(new TrustSelfSignedStrategy())
+                .build();
+
+        HttpPost createPerson = new HttpPost("https://people-api1.herokuapp.com/api/person");
+        JSONObject payLoadasObject = new JSONObject();
+        payLoadasObject.put("name","Pero");
+        payLoadasObject.put("surname","Blazhevski");
+        payLoadasObject.put("age",24);
+        payLoadasObject.put("isEmployed",true);
+        payLoadasObject.put("location","skopje");
+
+        String payLoadAsString = payLoadasObject.toString();
+
+        createPerson.setHeader(contentType);
+        createPerson.setEntity(new StringEntity(payLoadasObject.toString()));
+
+        HttpClient httpClient = HttpClients.custom().setSSLContext(sslContext).build();
+
+        HttpResponse response = httpClient.execute(createPerson);
+
+        HttpEntity entityPeople = response.getEntity();
+        String bodyPeople = EntityUtils.toString(entityPeople);
+
+        HttpEntity newEntity = new StringEntity(bodyPeople, ContentType.get(entityPeople));
+        response.setEntity(newEntity);
+
+        return response;
+
 
     }
 }
